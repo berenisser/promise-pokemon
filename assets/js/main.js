7ff7570b8b13e8cf16bf4cd58affc0e0cf181e29
berenisser <berenisser@gmail.com>
2017-07-12 23:37:15 -0400

    Tipo de pokemon incluido

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,19 +16,38 @@ var llamarHabilidades = function(urlHabilidades){
 	.done(function(respuesta){
 		console.log(respuesta.types[0].type.name);
 		console.log(respuesta.types.length);
-		var type = respuesta.types[0].type.name;
+
+		var type2;
+		if(respuesta.types.length==1){
+			type2 = respuesta.types[0].type.name;
+		} else{
+			type2 = respuesta.types[0].type.name + " / " +respuesta.types[1].type.name;
+		}
+
 		var nombreID = respuesta.name;
+		var type;
+
+		$("#"+nombreID).append('<div class="text-center poder">'+type2+'</div>');
+
+
+		respuesta.types.forEach(function(el){
+			console.log(el.type.name);
+			type = el.type.name;
+		})
 
         switch (type) { 
-            case 'poison': 
-                $("#"+nombreID).css("background-color","#78C850");
+            case 'poison' && 'grass': 
+                $("#"+nombreID).css("background","linear-gradient(90deg, #78C850 50%, #A040A0 50%)");
                 break;
             case 'fire': 
                 $("#"+nombreID).css("background-color","#F08030");
                 break;
             case 'flying': 
                 $("#"+nombreID).css("background-color","#A890F0");
-                break;      
+                break; 
+            case 'flying' && 'fire': 
+                $("#"+nombreID).css("background","linear-gradient(90deg, #F08030 50%, #A890F0 50%)");
+                break;  
             case 'water': 
                 $("#"+nombreID).css("background-color","#6890F0");
                 break;
@@ -36,8 +55,8 @@ var llamarHabilidades = function(urlHabilidades){
                 $("#"+nombreID).css("background-color","#A8B820");
                 break;
         }
-
 	})
+
 	.done(function(respuesta) {
 		console.log(respuesta.abilities);
 		var nombreID = respuesta.name;
@@ -45,13 +64,13 @@ var llamarHabilidades = function(urlHabilidades){
 		respuesta.abilities.forEach(function(el){
 			console.log(el.ability.name);
 			var abilityName = el.ability.name;
-			$("#"+nombreID).append('<p>Poder: '+abilityName+'</p>');
+			$("#"+nombreID).append('<span class="text-center">'+abilityName+' </span>');
 		})
 	})
 	.done(function(respuesta) {
 		console.log(respuesta.weight);
 		var nombreID = respuesta.name;
-		$("#"+nombreID).append('<p>Peso: '+ (respuesta.weight)/10+' kg</p>');
+		$("#"+nombreID).append('<p class="text-center">Peso: '+ (respuesta.weight)/10+' kg</p>');
 	})
 	
 	.fail(function() {
@@ -67,15 +86,15 @@ var llamarAjax = function(){
 		url: 'http://pokeapi.co/api/v2/pokemon',
 		type: 'GET',
 		dataType: 'json',
-		data: {'limit': '15'},
+		data: {'limit': '16'},
 	})
 	.done(function(respuesta) {
 		
 		console.log(respuesta);
 		respuesta.results.forEach(function(el){
 			var nombrePoke = el.name;
-			$(".all-pokemon").append('<div class="container-cada-poke col-lg-4 col-md-4 col-sm-4 col-xs-4">'+
-					'<div class="cada-poke col-lg-12 col-md-12 col-sm-12 col-xs-12" id="'+nombrePoke+'">'+
+			$(".all-pokemon").append('<div class="container-cada-poke col-lg-3 col-md-3 col-sm-4 col-xs-4">'+
+					'<div class="cada-poke text-center col-lg-12 col-md-12 col-sm-12 col-xs-12" id="'+nombrePoke+'">'+
 						'<h3 class="capitalizar text-center">'+nombrePoke+'</h3>'+
 					'</div>'+
 				'</div>').append(llamarHabilidades(el.url));
@@ -91,6 +110,7 @@ var llamarAjax = function(){
 }
 
 llamarAjax();
+
 //Imprimir pokemon + habilidad 10ptos X
 //Imprimir pokemon + todas las habilidades 15puntos X
 //Imprimir pokemon habilidades y cualquier otra cosas del pokemon +20ptos
